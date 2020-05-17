7d4ac9b157e42f9e50c5d37091fbcf4a6ad23d52
Lâm <vhlam1997@gmail.com>
2020-05-17 19:19:36 +0700

    add enum and model types

---
+++ src/types/enum.ts
@@ -0,0 +1,8 @@
+export enum collectionsEnum {
+  calendarDetail = "calendarDetail",
+  reviews = "review",
+  reviewsDetail = "reviewsDetail",
+  listings = "listings",
+  listingsDetail = "listingsDetail",
+  neighbourhoods = "neighbourhoods",
+}
