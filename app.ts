d071ff03cab912eb73aa134110d096de6f1b933e
Lâm <vhlam1997@gmail.com>
2020-07-14 23:47:23 +0700

    add api getlistingbyid

--- app.ts
+++ app.ts
@@ -7,10 +7,15 @@ import { getRecommendForAllUser } from "./src/algorithm/raccoon/utils";
 import { getReviewWithFakedData } from "./src/data/review/review";
 
 import raccoon from "./src/algorithm/raccoon/raccoon";
+import { getReviewers } from "./src/data/reviewer/reviewer";
+import { getListings, getListingById } from "./src/data/listing/listing";
 
 const app = express();
 const port = process.env.PORT || 4000;
 const baseUrl = `localhost:${port}`;
+
+const LIMIT_USER = process.env.LIMIT_USER;
+const LIMIT_LISTING = process.env.LIMIT_LISTING;
 
 raccoon();
 
@@ -35,11 +40,46 @@ app.get("/data/raccoon", (req, res, next) => {
   });
 });
 
-app.get("/data/users", (req, res, next) => {
-  return 1;
+app.get("/data/users", async (req, res, next) => {
+  if (!LIMIT_USER) {
+    next("no limit user");
+  }
+
+  if (LIMIT_USER) {
+    const data = await getReviewers(+LIMIT_USER);
+    res.send(data);
+    console.log("Request", req.originalUrl, "data", data.length, "users");
+  }
 });
 
 // get recommendation for user
 app.post("/data/recommend/userId", (req, res, next) => {
   return null;
+});
+
+// get rooms (listing)
+app.get("/data/listings", async (req, res, next) => {
+  if (!LIMIT_LISTING) {
+    next("no limit listing config");
+  }
+
+  if (LIMIT_LISTING) {
+    const data = await getListings(+LIMIT_LISTING);
+    res.send(data);
+    console.log("Request", req.originalUrl, "data", data.length, "listings");
+  }
+  return null;
+});
+
+app.get("/data/listing", async (req, res, next) => {
+  const { id } = req.query;
+  console.log("param", id);
+
+  if (!id) {
+    next();
+  }
+
+  const data = await getListingById(+id);
+  console.log("Request", req.originalUrl, "data");
+  res.send(data);
 });
--- src/data/listing/listing.ts
+++ src/data/listing/listing.ts
@@ -1,6 +1,7 @@
 import { listingDetail } from "../../types/listingDetail";
 import { getDataByCollection } from "../../utils/mongo";
 import { collectionsEnum } from "../../types/enum";
+import { collection } from "../../utils/mongo";
 
 export const getListings = async (
   limit: number = 1000
@@ -9,4 +10,15 @@ export const getListings = async (
     collection: collectionsEnum.listingDetails,
     limit,
   });
+};
+
+export const getListingById = async (id: number): Promise<listingDetail> => {
+  const col = await collection(collectionsEnum.listingDetails);
+
+  const data = await col.findOne({ id });
+
+  if (!data) {
+    throw new Error(`Listing ${id} not found`);
+  }
+  return data;
 };
